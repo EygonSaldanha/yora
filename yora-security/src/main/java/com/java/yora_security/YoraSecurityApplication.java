c0054ca91f6270c6f5bde294d5bf06fa7987dbdf
eygon saldanha <eygon11@gmail.com>
2024-06-11 12:32:39 -0300

    new: configuracoes iniciais de seguranca

---
+++ yora-security/src/main/java/com/java/yora_security/YoraSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.java.yora_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoraSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoraSecurityApplication.class, args);
+	}
+
+}
---
+++ yora-security/src/test/java/com/java/yora_security/YoraSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.yora_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoraSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
